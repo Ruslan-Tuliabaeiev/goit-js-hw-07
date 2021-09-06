dcf3ce3d24310abfc10a4653da92ff9454067dca
руслан <82108672+Ruslan-Tuliabaeiev@users.noreply.github.com>
2021-09-06 20:13:33 +0300

    dz2

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,7 @@
-document.querySelectorAll(".item h2").forEach(elem => console.log(`Категория: ${elem.textContent}, Количество элементов: ${elem.nextElementSibling.children.length}`));
 
+ document.querySelectorAll('#categories').forEach(elem => console.log( `В списке ${elem.children.length} категории.`));
+
+
+document.querySelectorAll(".item h2").forEach(elem => console.log(`Категория: ${elem.textContent}, Количество элементов: ${elem.nextElementSibling.children.length}`));
 
 
--- js/task-3.js
+++ js/task-3.js
@@ -1,40 +1,36 @@
 
-
-
 const images = [
-    {
-      url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'White and Black Long Fur Cat',
-    },
-    {
-      url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-    },
-    {
-      url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'Group of Horses Running',
-    },
-  ];
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
 
-const gallery = document.querySelector('#gallery');
-
-const fotoGallery = ({url, alt}) => {
-return `<li> <img src="${url}" alt="${alt}" width="450px" height="300px"></LI>`
-};
-
-const listGalleryFoto = images.map(fotoGallery).join('');
+  
 
 
+  const gallery = document.querySelector('#gallery');
+  
+const fotoGallery = elements => {return elements.map(({url,alt}) => {
+  
+    return `<li><img src="${url}" alt="${alt}" width="450px" height="300px"></li>`
 
-  gallery.insertAdjacentHTML('beforeend', listGalleryFoto);
- 
+  }).join('')
+}
+
 
 
 gallery.insertAdjacentHTML('afterbegin',fotoGallery(images));
-
 gallery.style.marginTop = '40px';
 gallery.style.display = 'flex';
 gallery.style.justifyContent = 'space-around';
 gallery.style.padding = '0';
 gallery.style.listStyle = 'none';
-
